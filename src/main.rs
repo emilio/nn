b04098a9eeeaada57c7b4bdd079baf3cc703493e
Emilio Cobos Álvarez <emilio@crisal.io>
2017-03-23 17:47:49 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#![feature(link_args)]
-
 extern crate rand;
 
 use rand::Rng;
@@ -84,10 +82,12 @@ impl Layer {
                    bias: f32,
                    rng: &mut R) -> Self {
         let mut neurons = Vec::with_capacity(neuron_count);
-        for i in 0..neuron_count {
+        for _ in 0..neuron_count {
             neurons.push(Neuron::new(bias, input_count, rng))
         }
-        Layer { neurons }
+        Layer {
+            neurons: neurons,
+        }
     }
 }
 
@@ -174,26 +174,196 @@ impl NeuralNetwork {
         output
     }
 
-    fn train(&mut self, one_input: &[f32], expected_output: &[f32]) {
+    fn train<Data>(&mut self, training_data: &[Data])
+        where Data: TrainingData,
+    {
+        for data in training_data {
+            self.train_one(data);
+        }
+    }
 
+    fn train_one<Data>(&mut self, training_data: &Data)
+        where Data: TrainingData,
+    {
+        let input = data.input();
+        let output = self.feed(input);
+        let expected_output = data.output();
+
+        assert_eq!(output.len(), expected_output.len());
+        let mut total_error = Vec::with_capacity(output.len());
+        for (actual, expected) in output.iter().zip(expected_output.iter()) {
+            error.push(expected - actual);
+        }
+
+        // Update the weights in the output layer.
+        for (i, neuron) in self.layers.last_mut().unwrap().neurons.iter_mut().enumerate() {
+            for (j, weight) in neuron.weights.iter_mut().enumerate() {
+                let partial_error = total_error[i] * neuron.last_input[j];
+                *weight -= self.learning_rate * partial_error;
+            }
+        }
+
+        for layer in self.layers.iter_mut().rev().skip(1) {
+            let mut partial_error = Vec::with_capacity(layer.neurons.len());
+
+            for neuron in &layer.neurons {
+                // We need to calculate the derivative of the error with respect
+                // to the output of each hidden layer neuron.
+                let mut error_
+            }
+        }
     }
 }
 
 
 struct MNISTTestImageIterator {
-    images: fs::File,
-    labels: fs::File,
+    images: io::Bytes<fs::File>,
+    labels: io::Bytes<fs::File>,
+    count: usize,
+    rows: usize,
+    columns: usize,
+}
+
+fn next_byte(bytes: &mut io::Bytes<fs::File>) -> io::Result<u8> {
+    match bytes.next() {
+        Some(b) => Ok(b?),
+        None => {
+            Err(io::Error::new(io::ErrorKind::UnexpectedEof,
+                               "Expected one byte at least"))
+        }
+    }
+}
+
+fn read_u32(bytes: &mut io::Bytes<fs::File>) -> io::Result<u32> {
+    let first = next_byte(bytes)?;
+    let second = next_byte(bytes)?;
+    let third = next_byte(bytes)?;
+    let fourth = next_byte(bytes)?;
+
+    Ok((first as u32) << 24 |
+       (second as u32) << 16 |
+       (third as u32) << 8 |
+       (fourth as u32))
 }
 
 impl MNISTTestImageIterator {
     fn new(mnist_path: &path::Path) -> io::Result<Self> {
-        Ok(Self {
-            images: fs::File::open(mnist_path.join("train-images-idx2-ubyte"))?,
-            labels: fs::File::open(mnist_path.join("train-labels-idx1-ubyte"))?,
-        })
+        use std::io::Read;
+        Self {
+            images: fs::File::open(mnist_path.join("train-images-idx3-ubyte"))?.bytes(),
+            labels: fs::File::open(mnist_path.join("train-labels-idx1-ubyte"))?.bytes(),
+            count: 0,
+            rows: 0,
+            columns: 0,
+        }.init()
+    }
+
+
+    fn init(mut self) -> io::Result<Self> {
+        // TODO(emilio): Return errors instead of asserting.
+        let magic = read_u32(&mut self.labels)?;
+        assert_eq!(magic, 2049);
+
+        let magic = read_u32(&mut self.images)?;
+        assert_eq!(magic, 2051);
+
+        let label_count = read_u32(&mut self.labels)?;
+        let image_count = read_u32(&mut self.images)?;
+
+        assert_eq!(label_count, image_count);
+        self.count = label_count as usize;
+        self.rows = read_u32(&mut self.images)? as usize;
+        self.columns = read_u32(&mut self.images)? as usize;
+
+        Ok(self)
+    }
+}
+
+trait TrainingData {
+    fn input(&self) -> &[f32];
+    fn output(&self) -> &[f32];
+}
+
+#[derive(Debug)]
+struct MNISTLabeledImage {
+    pixels: Vec<f32>, // Bytes normalized from 0..255 to 0.0..1.0
+    expected_output: Vec<f32>,
+    label: u8,
+    rows: usize,
+    columns: usize,
+}
+
+impl TrainingData for MNISTLabeledImage {
+    fn input(&self) -> &[f32] {
+        &self.pixels
+    }
+    fn output(&self) -> &[f32] {
+        &self.expected_output
+    }
+}
+
+impl
+
+impl Iterator for MNISTTestImageIterator {
+    type Item = io::Result<MNISTLabeledImage>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count == 0 {
+            return None;
+        }
+        self.count -= 1;
+
+        let label = match self.labels.next() {
+            Some(Ok(l)) => l,
+            Some(Err(e)) => {
+                self.count = 0;
+                return Some(Err(e));
+            }
+            None => {
+                self.count = 0;
+                return None;
+            }
+        };
+
+        let mut pixels = Vec::with_capacity(self.rows * self.columns);
+        for _ in 0..self.rows {
+            for _ in 0..self.columns {
+                let pixel = match self.images.next() {
+                    Some(Ok(l)) => l,
+                    Some(Err(e)) => {
+                        self.count = 0;
+                        return Some(Err(e));
+                    }
+                    None => {
+                        self.count = 0;
+                        return None;
+                    }
+                };
+
+                pixels.push(pixel as f32 / 255.0)
+            }
+        }
+
+        assert!(label >= 0 && label <= 9);
+        let mut output = vec![0.0; 10];
+        output[label] = 1.0;
+
+        Some(Ok(MNISTLabeledImage {
+            label: label,
+            expected_output: output,
+            pixels: pixels,
+            rows: self.rows,
+            columns: self.columns,
+        }))
     }
 }
 
 fn main() {
     // http://yann.lecun.com/exdb/mnist/
+    let mnist_path = path::Path::new("./mnist");
+    let images = MNISTTestImageIterator::new(&mnist_path).unwrap();
+    let mut count = 0;
+    for _image in images {
+        count += 1;
+    }
 }
